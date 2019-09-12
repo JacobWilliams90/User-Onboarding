11d15682ca1ad34ab2e575deb51c20ec1a2d6344
Jacob Williams <JaWilliamsDev@gmail.com>
2019-09-12 18:55:08 -0400

    started stylign, updating form with new fields.

--- User-Onboadring/src/Components/Form.js
+++ User-Onboadring/src/Components/Form.js
@@ -13,28 +13,40 @@ const UserForm = ({errors, touched, status}) => {
     }, [status])
 
     return (
+        <>
         <Form>
+            <p>User Onboarding</p>
             {touched.name && errors.name && <p className='error'>{errors.name}</p>}
             <Field type='text' name='name' placeholder='Name'/>
+            {touched.role && errors.role && <p className='error'>{errors.role}</p>}
+            <Field name='role' component='select'>
+                <option value='' name='nothing' disabled>Select Role:</option>
+                <option name='admin'>Admin</option>
+                <option name='user'>User</option>
+            </Field>
             {touched.email && errors.email && <p className='error'>{errors.email}</p>}
             <Field type='text' name='email' placeholder='E-Mail'/>
             {touched.password && errors.password && <p className='error'>{errors.password}</p>}
             <Field type='text' name='password' placeholder='Password'/>
-            {touched.tos && errors.tos && <p className='error'>{errors.tos}</p>}
+            {touched.botcheck && errors.botcheck && <p className='error'>{errors.botcheck}</p>}
+            <Field type='number' name='botcheck' placeholder='What is 2+2' />
+            {errors.tos && <p className='error'>{errors.tos}</p>}
             <label>
                 <Field type='checkbox' name='tos' />
                 <span>Terms Of Service</span>
             </label>
-            <button type='submit'>Submit</button>
+            <button type='submit' name='submit'>Submit</button>
+        </Form>
 
         {users.map((users) => (
             <div>
                 Name: {users.name}<br />
+                Role: {users.role}<br />
                 E-mail: {users.email}<br />
                 Password: Wouldnt you like to know!<br />
             </div>
         ))}
-        </Form>
+    </>
     )
 }
 
@@ -42,22 +54,24 @@ export default withFormik({
     mapPropsToValues: (values) => {
         return {
             name: values.name || '',
+            role: values.role || '',
             email: values.email || '',
             password: values.password || '',
+            botcheck: values.botcheck || '',
             checkbox: values.checkbox || false,
-
         }
     },
     validationSchema: yup.object().shape({
         name: yup.string().required('Name Is A Required Field.'),
-        email: yup.string().required('E-mail Is required'),
+        role: yup.string().required('Please Select your user Level'),
+        email: yup.string().email('E-mail must be a valid address').required('E-mail Is required'),
         password: yup.string().required('Did you forget your password?'),
-        tos: yup.boolean().oneOf([true], 'You must accept the terms of service to continue!')
+        botcheck: yup.number(4).required('You answered incorrectly').positive(),
+        tos: yup.boolean().oneOf([true],'You must accept the terms of service to continue!')
     }),
     handleSubmit: (values, { setStatus }) => {
         axios.post('https://reqres.in/api/users', values)
         .then(res => {
-            console.log(res)
             setStatus(res.data)
         })
         .catch(err => {
